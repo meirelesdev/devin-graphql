cb6f7ca550c9de8c6a95c07bebe814002c206360
Daniel Meireles <httpdaniel@yahoo.com.br>
2022-05-15 11:28:03 -0300

    Finalizando mutations para posts

--- src/resolver/index.js
+++ src/resolver/index.js
@@ -32,6 +32,15 @@ const getPosts = (_, { search, order }) => {
   return result;
 };
 
+const likeOrDesLikePost = (id, like) =>{
+  const index = posts.findIndex(post => post.id === Number(id))
+  if(like) {
+    posts[index].likes++
+    return posts[index]
+  }
+  posts[index].likes--
+    return posts[index]
+}
 const resolvers = {
   Query: {
     posts: getPosts,
@@ -39,11 +48,68 @@ const resolvers = {
     users: () => users,
     user: (_, { id }) => users.find((user) => user.id === Number(id)),
   },
+  Mutation: {
+    createPost: (_, {title, description, body, authorId}) => {
+      const user = users.find(user => user.id === Number(authorId))
+      const post = {
+        id: posts.length + 1,
+        title,
+        description,
+        body,
+        author: user.id,
+        likes: 0,
+        createdAt: new Date()
+      }
+      user.posts.push(post.id)
+      posts.push(post)
+      return post
+    },
+    updatePost:(_, {id, title, description, body, authorId}) => {
+      const post  = posts.find(post => post.id === Number(id))
+      if(post) {
+        post.title = title || post.title
+        post.description = description || post.description
+        post.body = body || post.body
+        post.author = Number(authorId) || post.author
+      }
+      return post      
+    },
+    removePost:(_, { id }) => {
+      const index = posts.findIndex((post)=> post.id === Number(id))
+      if(index > -1) {
+        const [ deletePost ] = posts.splice(index, 1)
+        return deletePost
+      }      
+    },
+    addLikePost:(_, { id } ) => {
+      return likeOrDesLikePost(id, true)
+    },
+    removeLikePost:(_, { id }) => {
+      return likeOrDesLikePost(id, false)
+    },
+    createUser:(name) => {
+      
+    },
+    updateUser:(id, title, description, body, authorId) => {
+      
+    },
+    followUser:(id, followerId) => {
+      
+    },
+    removeUser:(id) => {
+      
+    },
+  },
   Post: {
       author: (parent) => users.find(user => user.id === Number(parent.author))
   },
   User: {
-    posts: (parent) => posts.filter(post => parent.posts.includes(post.id)),
+    posts: (parent) => {
+      console.log(posts)
+      const postsOfAuthor = posts.filter(post => parent.posts.includes(post.id))
+      console.log(postsOfAuthor)
+      return postsOfAuthor
+    },
     followers: (parent) => users.filter(follower => parent.followers.includes(follower.id))
   }
 };
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,9 +3,9 @@ const { gql } = require("apollo-server");
 const schema = gql`
   type Query {
     "O campo \'search\' é um texto livre para busca, o campo 'order' pode receber somente duas opções: \'data\' ou \'likes\'"
-    posts( search: String, order: String ): [Post!],
-    post(id: ID): Post!
-    users: [User!],
+    posts( search: String, order: String ): [Post]!,
+    post(id: ID): Post
+    users: [User]!,
     user(id: ID): User!
   }
 
